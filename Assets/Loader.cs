4123a9c0ceac2151d81491b4464cd7112304c136
srb76 <bonnersa@oregonstate.edu>
2019-10-07 16:13:48 -0700

    Added splash screen and persistent music

---
+++ Assets/Loader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loader : MonoBehaviour
+{
+    Text text;
+    
+    void Start()
+    {
+        //Get text object
+        text = GameObject.FindObjectOfType<Text>();
+    }
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            StartCoroutine(LoadLevel());
+            text.text = "Loading...";
+        }
+    }
+
+    IEnumerator LoadLevel()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Level 1");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        
+    }
+}
