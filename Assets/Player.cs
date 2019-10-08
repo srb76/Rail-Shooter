f542e66779a13a472bdb98b7c0c3b686dadc3715
srb76 <bonnersa@oregonstate.edu>
2019-10-07 17:26:16 -0700

    Added flythrough, water, unity standard assets

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    float horizontalThrow;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        print(horizontalThrow);
+    }
+}
