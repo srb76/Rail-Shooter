2999d3b6828b3e7ab9e0727db3ae61772266023f
srb76 <bonnersa@oregonstate.edu>
2019-10-22 14:35:26 -0700

    Score is now incremented on hitting an enemy, lowered music volume slightly

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,13 +6,18 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int ScorePerHit = 1;
     Collider boxCollider;
+    ScoreBoard scoreboard;
     // Start is called before the first frame update
     void Start()
     {
         //add box collider
         boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
+
+        //add ref to scoreboard
+        scoreboard = FindObjectOfType<ScoreBoard>();
     }
 
     // Update is called once per frame
@@ -23,7 +28,9 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
-        //Instantiate(deathFX, gameObject.transform);
+        //update score
+        scoreboard.addScore(ScorePerHit);
+
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
--- Assets/ScoreBoard.cs
+++ Assets/ScoreBoard.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreBoard : MonoBehaviour
 {
+    int score;
+    Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void addScore(int scorePerHit)
     {
-        
+        score += scorePerHit;
+        //update text
+        scoreText.text = score.ToString();
     }
 }
