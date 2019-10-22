2a6cb9d3de8d59ebecbef46173e0b8dcfa419874
srb76 <bonnersa@oregonstate.edu>
2019-10-22 13:50:42 -0700

    Added enemy explosions and destruction, Added placeholder scoreboard

---
+++ Assets/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    Collider boxCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //add box collider
+        boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        //Instantiate(deathFX, gameObject.transform);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
