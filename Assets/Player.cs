aa1c5c88ce2470926a7986b700e3dadd20852e7c
srb76 <bonnersa@oregonstate.edu>
2019-10-21 20:59:45 -0700

    Added obstacles, sfx, static enemies, placeholder shooting particles and death explosion. Also limited movement and rotated player ship based on input.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,22 +1,80 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    float horizontalThrow;
-    
+    float xThrow, yThrow;
+    float xOffset, yOffset;
+    float rawX, rawY;
+
+    [Tooltip("In m/s")][SerializeField] float xSpeed = 25f;
+    [SerializeField] float xRange = 15f;
+    [Tooltip("In m/s")] [SerializeField] float ySpeed = 25f;
+    [SerializeField] float yRange = 8f;
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 3f;
+    [SerializeField] float controlRollFactor = -20f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Player collided with something!");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("Player triggered something!");
+    }
+
     // Update is called once per frame
     void Update()
     {
-        horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch, yaw, roll;
+
+        //couple pitch to position on screen AND control throw
+        float pitchPosition = transform.localPosition.y * positionPitchFactor; //couple y POSITION to pitch
+        float pitchControl = yThrow * controlPitchFactor;
+        pitch = pitchPosition + pitchControl;
+
+        //couple yaw to position on screen
+        yaw = transform.localPosition.x * positionYawFactor;
+
+        //couple roll to control throw
+        roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xOffset = xThrow * xSpeed * Time.deltaTime;
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        //find new x position
+        rawX = transform.localPosition.x + xOffset;
+        rawX = Mathf.Clamp(rawX, -xRange, xRange);
+
+        rawY = transform.localPosition.y + yOffset;
+        rawY = Mathf.Clamp(rawY, -yRange, yRange);
+
+        transform.localPosition = new Vector3(rawX, rawY, transform.localPosition.z);
     }
 }
